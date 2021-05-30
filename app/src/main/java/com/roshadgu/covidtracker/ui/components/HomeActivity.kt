a01bf83d60fc9f0f18a8bef3cd3d7fdb1be298a9
Roshadgu <roshadgu@gmail.com>
2021-05-30 00:03:08 -0400

    Incorporating Robinhood SparkView in UI

--- app/src/main/java/com/roshadgu/covidtracker/ui/components/HomeActivity.kt
+++ app/src/main/java/com/roshadgu/covidtracker/ui/components/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.roshadgu.covidtracker.ui.components
 
+import android.content.Context
 import android.os.Bundle
-import android.text.style.BackgroundColorSpan
-import android.widget.ImageView
-import android.widget.RadioGroup
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.*
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,18 +15,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
 import com.roshadgu.covidtracker.ui.theme.CovidTrackerTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement.Horizontal
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.constraintlayout.compose.ConstraintLayout
-
+import com.robinhood.spark.SparkView
+import com.robinhood.ticker.TickerView
 
 class HomeActivity : AppCompatActivity()
 {
@@ -47,7 +46,7 @@ fun HomeScreen()
   ConstraintLayout(modifier = Modifier.fillMaxSize())
   {
     val radioOptions = listOf("Negative", "Positve", "Death") //edit later
-    val (tvSelectState, RadioGrpMetricSelection, RadioGrpTimeSelection) = createRefs()
+    val (tvSelectState, RadioGrpMetricSelection, RadioGrpTimeSelection, RHSparkView, RHTickerView, DateLabel) = createRefs()
     val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
 
     Text(
@@ -80,8 +79,21 @@ fun HomeScreen()
       Text(text = "Death")
     }
 
-    val imageView = findViewById(R.id.myimageview) as ImageView
-    imageView.setImageResource(R.layout.robinhood_sparkview)
+    //Robhinhood Spark View
+    AndroidView(
+      modifier = Modifier
+        .fillMaxSize()
+        .constrainAs(RHSparkView)
+        {
+          top.linkTo(RadioGrpMetricSelection.bottom)
+          bottom.linkTo(RadioGrpTimeSelection.top)
+        },
+      factory = { context ->
+        SparkView(context).apply {
+          ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+        }
+      }
+    )
 
     Row( //Time Selection
       Modifier
@@ -104,38 +116,23 @@ fun HomeScreen()
       RadioButton(selected = false, onClick = { /*TODO*/ }, Modifier.padding(start = 3.dp, end = 3.dp))
       Text(text = "Max")
     }
-
-    /*Column(
-      Modifier
-        .constrainAs(RadioGrp)
-        {
-          top.linkTo(tvSelectState.bottom)
-          start.linkTo(parent.start)
-          end.linkTo(parent.end)
-        }
-        .selectableGroup()) {
-        radioOptions.forEach{ text ->
-          Row(
-            Modifier
-              .fillMaxWidth()
-              .height(56.dp)
-              .selectable(
-                selected = (text == selectedOption),
-                onClick = { onOptionSelected(text) },
-                role = Role.RadioButton
-              )
-              .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-          ) {
-            RadioButton(selected = (text == selectedOption), onClick = null)
-            Text(text = text, style = MaterialTheme.typography.body1.merge(), modifier = Modifier.padding(start = 16.dp)
-            )
-
-          }
-      }
-    }*/
   }
+
+  BoxWithConstraints()
+  {
+    Text(text = "May 31, 2020")
+  }
+
+  //Robinhood Ticker View
+  AndroidView(
+    modifier = Modifier
+      .fillMaxSize()
+      .constrainAs(RHTickerView)
+      {
+        //start.linkto(parent.end)
+      },
+    factory = { context -> TickerView(context).apply { } }
+  )
 }
 
 @Preview(showBackground = true, backgroundColor = 0xffffff)
